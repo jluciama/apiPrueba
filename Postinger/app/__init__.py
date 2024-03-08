b43a94303b9dba393a380d5dd013a6c151f47b7d
jluciama <jluciama@emeal.nttdata.com>
2024-03-08 12:53:47 +0100

    08/03/2024
    ay madre

---
+++ Postinger/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SECRET_KEY'] = '42dc81b62afae38f19617162c1e3e666'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+
+from app import routes
+
+
+# Create database tables within application context
+with app.app_context():
+    db.create_all()
