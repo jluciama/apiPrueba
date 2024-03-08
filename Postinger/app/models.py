4e247be0f34faee728b5359bb11cbc9c99e72a15
jluciama <jluciama@emeal.nttdata.com>
2024-03-08 12:46:30 +0100

    08/03/2024
    name change

---
+++ Postinger/app/models.py
@@ -0,0 +1,44 @@
+from app import db, bcrypt
+from flask_login import UserMixin
+
+# Association table for likes
+likes = db.Table('likes',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
+
+# Association table for dislikes
+dislikes = db.Table('dislikes',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(), nullable=False)
+    email_address = db.Column(db.String(), nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
+    posts = db.relationship('Post', backref='owned_user', lazy=True)
+
+    def set_password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    owned_user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    tags = db.Column(db.String())
+    likes_count = db.Column(db.Integer(), default=0)
+    dislikes_count = db.Column(db.Integer(), default=0)
+
+    # Define relationships with users for likes and dislikes
+    liked_users = db.relationship('User', secondary=likes, backref=db.backref('liked_posts', lazy='dynamic'))
+    disliked_users = db.relationship('User', secondary=dislikes, backref=db.backref('disliked_posts', lazy='dynamic'))
+
+    def __repr__(self):
+        return self.title
